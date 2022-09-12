aa128fdd27df1f7dbb5bd7c333f5980d80c853b0
SamuelSanchezAtehortua <105441967+SamuelSanchezAtehortua@users.noreply.github.com>
2022-09-12 16:04:15 -0500

    Add files via upload

---
+++ clase-jueves-main/Ejercicio De Clase/data/data.js
@@ -0,0 +1,33 @@
+export const data =
+[
+    {
+        "id":1,
+        "name":"Tío Rico",
+        "image":"./imagenes/imagen1.png"
+    }, 
+    {
+        "id":2,
+        "name":"Tío Rico",
+        "image":"./imagenes/imagen2.png"
+    },
+    {
+        "id":3,
+        "name":"Tío Rico",
+        "image":"./imagenes/imagen3.png"
+    },
+    {
+        "id":4,
+        "name":"Tío Rico",
+        "image":"./imagenes/imagen4.png"
+    },
+    {
+        "id":5,
+        "name":"Tío Rico",
+        "image":"./imagenes/imagen5.png"
+    },
+    {
+        "id":6,
+        "name":"Tío Rico",
+        "image":"./imagenes/imagen7.png"
+    },
+]
